03012a8cbeddaccb411de660394744cff89b913e
WeiHungLoh <weihung789@hotmail.com>
2025-03-11 13:04:28 +0800

    Remove trailing space

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -60,6 +60,6 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
-        attendanceIcon.setFill(Color.GREEN);            
+        attendanceIcon.setFill(Color.GREEN);
     }
 }
