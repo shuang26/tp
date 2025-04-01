01ae0d9ba72364b41fceed6c36d8f157b4515c35
WeiHungLoh <weihung789@hotmail.com>
2025-04-02 01:01:45 +0800

    Fix bugs in markall and unmarkall

--- src/main/java/seedu/address/logic/commands/MarkAllCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkAllCommand.java
@@ -2,9 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_ALL_STUDENT_ATTENDANCE_MARKED;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_STUDENTS;
+
+import java.util.List;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.student.Student;
 
 /**
  * Marks all students in the students list as present.
@@ -12,18 +16,22 @@ import seedu.address.model.Model;
 public class MarkAllCommand extends Command {
     public static final String COMMAND_WORD = "markall";
 
-    public static final String MESSAGE_EMPTY_STUDENT_LIST = "No students found in the student list";
-
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
+        List<Student> students = model.getAddressBook().getStudentList();
 
         if (students.isEmpty()) {
             throw new CommandException("No students found in the student list.");
         }
 
-        model.markAllStudents();
+        for (int i = 0; i < students.size(); i++) {
+            students.get(i).setPresent();
+        }
 
+        model.updateFilteredStudentList(student -> false);
+        model.updateFilteredStudentList(PREDICATE_SHOW_ALL_STUDENTS);
         return new CommandResult(MESSAGE_ALL_STUDENT_ATTENDANCE_MARKED);
     }
 }
