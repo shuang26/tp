c5f8b818719170d941a0d115f6f688977dbc8cdb
shuang26 <shuangyuan9@gmail.com>
2025-04-01 21:18:26 +0800

    Add test for unmarkAllCommand

--- src/test/java/seedu/address/logic/commands/UnmarkAllCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnmarkAllCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.Messages.MESSAGE_ALL_STUDENT_ATTENDANCE_UNMARKED;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
 
@@ -15,21 +16,27 @@ import seedu.address.model.student.Student;
 
 public class UnmarkAllCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private Model expectedModel = model;
-    // edit expectedModel
-
-
+    private Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
 
     @Test
-    public void execute() {
+    public void execute_unmarkall_success() {
         UnmarkAllCommand unmarkAllCommand = new UnmarkAllCommand();
         List<Student> students = model.getFilteredStudentList();
 
         for (Student student : students) {
-            student.setAbsent();
+            student.setPresent();
         }
 
         assertCommandSuccess(unmarkAllCommand, model, MESSAGE_ALL_STUDENT_ATTENDANCE_UNMARKED, expectedModel);
+    }
+
+    @Test
+    public void execute_emptyStudentList_throwsCommandException() {
+        // Create a model with an empty student list
+        UnmarkAllCommand unmarkAllCommand = new UnmarkAllCommand();
+
+        model = new ModelManager();
+        assertCommandFailure(unmarkAllCommand, model, UnmarkAllCommand.MESSAGE_EMPTY_STUDENT_LIST);
     }
 }
 
