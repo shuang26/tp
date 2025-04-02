23866ced6f9c654584b158f7a669b6f55e21d618
varuuuun <varun.s468a@gmail.com>
2025-04-02 16:59:11 +0800

    Fix usage message

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -21,7 +21,7 @@ public class MarkCommand extends Command {
     public static final String COMMAND_WORD = "mark";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Marks the student identified as present by the "
-            + "student id as present.\nParameters: [STUDENT_ID]\n"
+            + "student id as present.\nParameters: STUDENT_ID\n"
             + "Example: " + COMMAND_WORD + " A01A";
 
     private final StudentId id;
