95f79d8205ea309282ac61774576c25961de1fab
SahanaAvantika <sahanaavantika@gmail.com>
2025-04-03 19:02:58 +0800

    Improve message_usuage

--- src/main/java/seedu/address/logic/commands/EditStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/EditStudentCommand.java
@@ -35,15 +35,15 @@ public class EditStudentCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Edits the details of the student identified "
-            + "by the student ID used in the displayed student list. "
+            + "by the student ID used in the displayed student list. \n"
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: Student ID "
-            + "[" + PREFIX_STUDENT_NAME + "STUDENT NAME] "
-            + "[" + PREFIX_ID + "STUDENT ID] "
-            + "[" + PREFIX_PARENT_NAME + "PARENT NAME] "
+            + "[" + PREFIX_STUDENT_NAME + "STUDENT_NAME] "
+            + "[" + PREFIX_ID + "STUDENT_ID] "
+            + "[" + PREFIX_PARENT_NAME + "PARENT_NAME] "
             + "[" + PREFIX_PHONE + "PHONE] "
             + "[" + PREFIX_EMAIL + "EMAIL] "
-            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] \n"
             + "Example: " + COMMAND_WORD + " A01A "
             + PREFIX_PHONE + "91234567 "
             + PREFIX_EMAIL + "johndoe@example.com";
